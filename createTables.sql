31192e42203dc0696fffd69d60e3e065b5591191
LuccaHaddadSerejo <hslucca@gmail.com>
2023-02-10 14:09:53 -0300

    fix create tables order

--- createTables.sql
+++ createTables.sql
@@ -1,4 +1,10 @@
 CREATE TYPE osoption AS ENUM ('Windows', 'Linux', 'MacOS');
+	
+CREATE TABLE IF NOT EXISTS developers_info(
+	"id" SERIAL PRIMARY KEY,
+	"developerSince" DATE NOT NULL,
+	"preferredOS" osoption NOT NULL,
+);
 
 CREATE TABLE IF NOT EXISTS developers(
 	"id" SERIAL PRIMARY KEY,
@@ -7,18 +13,7 @@ CREATE TABLE IF NOT EXISTS developers(
 	"developerInfoID" INTEGER UNIQUE,
 	FOREIGN KEY ("developerInfoID") REFERENCES developers_info(id) ON DELETE CASCADE
 );
-	
-CREATE TABLE IF NOT EXISTS developers_info(
-	"id" SERIAL PRIMARY KEY,
-	"developerSince" DATE NOT NULL,
-	"preferredOS" osoption NOT NULL,
-);
-	
-CREATE TABLE IF NOT EXISTS projects_technologies(
-	"id" SERIAL PRIMARY KEY,
-	"addedIn" DATE NOT NULL
-);
-	
+		
 CREATE TABLE IF NOT EXISTS projects(
 	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR(50) NOT NULL,
@@ -26,11 +21,22 @@ CREATE TABLE IF NOT EXISTS projects(
 	"estimatedTime" VARCHAR(20) NOT NULL,
 	"repository" VARCHAR(120) NOT NULL,
 	"startDate" DATE NOT NULL,
-	"endDate" DATE NOT NULL
+	"endDate" DATE NOT NULL,
+	"developerId" INTEGER NOT NULL, 
+	FOREIGN KEY ("developerId") REFERENCES developers(id)
 );
 
 
 CREATE TABLE IF NOT EXISTS technologies(
 	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR(30) NOT NULL
+);
+
+   CREATE TABLE IF NOT EXISTS projects_technologies(
+	"id" SERIAL PRIMARY KEY,
+	"addedIn" DATE NOT NULL,
+	"projectId" INTEGER NOT NULL,
+	"technologyId" INTEGER NOT NULL,
+	FOREIGN KEY ("projectId") REFERENCES projects(id),
+	FOREIGN KEY ("technologyId") REFERENCES technologies(id)
 );
